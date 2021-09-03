3d8986e7610be018bb6004c6b8f8bd42024d53a4
sabya2001 <85751002+sabya2001@users.noreply.github.com>
2021-09-03 10:28:39 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,77 @@
+const bill = document.querySelector(".values1");
+const person = document.querySelector(".values2");
+const custom = document.querySelector(".Custom");
+let tipAmount1 = document.querySelector("#A1");
+let tipAmount2 = document.querySelector("#A2");
+let tipAmount3 = document.querySelector("#A3");
+let tipAmount4 = document.querySelector("#A4");
+let tipAmount5 = document.querySelector("#A5");
+let count = 1;
+tipAmount1.onclick = () => {
+
+    if (count == 0) {
+
+        tipAmount1.style.backgroundColor = "hsl(183, 100%, 15%)";
+        count = 1;
+    }
+    else {
+        tipAmount1.style.backgroundColor = "hsl(172, 67%, 45%)";
+        count = 0;
+    }
+    const tAmount = parseInt(tipAmount1.innerText);
+};
+tipAmount2.addEventListener('click', () => {
+
+    if (count == 0) {
+
+        tipAmount2.style.backgroundColor = "hsl(183, 100%, 15%)";
+        count = 1;
+    }
+    else {
+        tipAmount2.style.backgroundColor = "hsl(172, 67%, 45%)";
+        count = 0;
+    }
+    const tAmount = parseInt(tipAmount2.innerText);
+});
+tipAmount3.addEventListener('click', () => {
+
+    if (count == 0) {
+
+        tipAmount3.style.backgroundColor = "hsl(183, 100%, 15%)";
+        count = 1;
+    }
+    else {
+        tipAmount3.style.backgroundColor = "hsl(172, 67%, 45%)";
+        count = 0;
+    }
+    const tAmount = parseInt(tipAmount3.innerText);
+});
+tipAmount4.addEventListener('click', () => {
+
+    if (count == 0) {
+
+        tipAmount4.style.backgroundColor = "hsl(183, 100%, 15%)";
+        count = 1;
+    }
+    else {
+        tipAmount4.style.backgroundColor = "hsl(172, 67%, 45%)";
+        count = 0;
+    }
+    const tAmount = parseInt(tipAmount4.innerText);
+});
+tipAmount5.addEventListener('click', () => {
+
+    if (count == 0) {
+
+        tipAmount5.style.backgroundColor = "hsl(183, 100%, 15%)";
+        count = 1;
+    }
+    else {
+        tipAmount5.style.backgroundColor = "hsl(172, 67%, 45%)";
+        count = 0;
+    }
+    const tAmount = parseInt(tipAmount5.innerText);
+});
+
+
+
